e34c58464e3cf41627a898402fc4352e452287b4
Muhammad Asyrofuddien <asyrofuddien11@gmail.com>
2025-07-30 09:21:12 +0700

    fix(server): send cors headers

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -37,7 +37,8 @@ export default tseslint.config(
       '@typescript-eslint/no-unsafe-call': 'warn',
       '@typescript-eslint/no-unsafe-member-access': 'warn',
       '@typescript-eslint/require-await': 'warn',
-      '@typescript-eslint/no-unused-vars': 'warn'
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'prettier/prettier': 'off',
     },
   },
 );
